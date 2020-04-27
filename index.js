5bb8c296f5ec6d068797f5c5d460552dd8e12aaa
Nederby <nederby@gmail.com>
2020-04-27 22:48:12 +0200

    test3

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 var http = require('http');
 var httpProxy = require('http-proxy');
 var target = 'https://forms.dixa.io/v1/forms/734UO5aJSvUB6t12XJeV1S/2X7gnrIG6Fm53Wrwvh0D6g';
-const port = process.env.PORT || 8000;
+const port = 8000;
 
 //
 // Create your proxy server and set the target in the options.
 //
-const proxy = httpProxy.createProxyServer({target, changeOrigin: true}).listen(port);
+const proxy = httpProxy.createProxyServer({}).listen(8000);
 
 //
 // Create your target server
 //
 http.createServer(function (req, res) {
-    //proxy.web(req, res, { target: 'http://localhost:8085' });
+    proxy.web(req, res, {target, changeOrigin: true});
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
     res.end();
