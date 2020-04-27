71b520b143348abae2c80a6f4af055059aee7bbe
Nederby <nederby@gmail.com>
2020-04-27 22:34:52 +0200

    local

--- index.js
+++ index.js
@@ -1,32 +1,18 @@
 var http = require('http');
 var httpProxy = require('http-proxy');
-
-//
-// Create a proxy server with custom application logic
-//
-var proxy = httpProxy.createProxyServer({});
-
-// To modify the proxy connection before data is sent, you can listen
-// for the 'proxyReq' event. When the event is fired, you will receive
-// the following arguments:
-// (http.ClientRequest proxyReq, http.IncomingMessage req,
-//  http.ServerResponse res, Object options). This mechanism is useful when
-// you need to modify the proxy request before the proxy connection
-// is made to the target.
-//
-proxy.on('proxyReq', function(proxyReq, req, res, options) {
-    proxyReq.setHeader('X-Special-Proxy-Header', 'foobar');
-});
-
-var server = http.createServer(function(req, res) {
-  // You can define here your custom logic to handle the request
-  // and then proxy the request.
-    proxy.web(req, res, {
-        target: 'https://forms.dixa.io/v1/forms/734UO5aJSvUB6t12XJeV1S/2X7gnrIG6Fm53Wrwvh0D6g',
-        changeOrigin: true,
-    });
-});
+var target = 'https://forms.dixa.io/v1/forms/734UO5aJSvUB6t12XJeV1S/2X7gnrIG6Fm53Wrwvh0D6g';
 const port = process.env.PORT || 8000;
-console.log(`listening on port ${port}}`)
-server.listen(port);
 
+//
+// Create your proxy server and set the target in the options.
+//
+httpProxy.createProxyServer({target}).listen(port);
+
+//
+// Create your target server
+//
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
+  res.end();
+}).listen(9000);
